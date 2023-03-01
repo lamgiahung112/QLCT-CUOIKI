b4b3b1e15576e21f6f9bc8a09998dd8349b06d94
bananaback <votrongtin882003@gmail.com>
2023-03-02 00:02:20 +0700

    Save progress

--- CuoiKi/DAOs/EmployeeDAO.cs
+++ CuoiKi/DAOs/EmployeeDAO.cs
@@ -1,7 +1,32 @@
-﻿namespace CuoiKi.DAOs
+﻿using CuoiKi.Models;
+
+namespace CuoiKi.DAOs
 {
-    class EmployeeDAO
+    class EmployeeDAO : IDAO<Employee>
     {
+        private DBConnection dbc;
+        private SqlConverter sqlConverter;
+        public EmployeeDAO()
+        {
+            dbc = new DBConnection();
+            sqlConverter = new SqlConverter();
+        }
+        public void Add(Employee entry)
+        {
+            string command = "";
+            dbc.Execute(command);
+        }
+        public void Delete(Employee entry)
+        {
+            string command = "";
+            dbc.Execute(command);
+        }
+
+        public void Modify(Employee entry)
+        {
+            string command = "";
+            dbc.Execute(command);
+        }
     }
 }
 
