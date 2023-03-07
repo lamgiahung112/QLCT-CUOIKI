525421e14d40ebb823b3f7dc81f348e30f21e41a
bananaback <votrongtin882003@gmail.com>
2023-03-08 00:16:55 +0700

    Về bờ

--- CuoiKi/App.xaml.cs
+++ CuoiKi/App.xaml.cs
@@ -1,12 +1,4 @@
-﻿using CuoiKi.DAOs;
-using CuoiKi.Models;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace CuoiKi
 {
