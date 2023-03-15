fd7066b4f8c351e8bbc69db934f1a58f6209f766
bananaback <votrongtin882003@gmail.com>
2023-03-15 17:21:34 +0700

    allow manager form move

--- CuoiKi/UI/Manager/UI_ManagerForm.xaml.cs
+++ CuoiKi/UI/Manager/UI_ManagerForm.xaml.cs
@@ -1,17 +1,5 @@
-﻿using CuoiKi.UI.Staff;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace CuoiKi.UI.Manager
 {
@@ -48,6 +36,14 @@ namespace CuoiKi.UI.Manager
         private void btn_KPI_click(object sender, RoutedEventArgs e)
         {
             frameContent.Navigate(new KPIForm());
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            // Begin dragging the window
+            this.DragMove();
         }
     }
 }
