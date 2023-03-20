0ba5e9f2a6f4af75f6b6c5e8477b9bb479217ecd
bananaback <votrongtin882003@gmail.com>
2023-03-21 00:31:59 +0700

    Add missed code

--- CuoiKi/ViewModels/StagesPageViewModel.cs
+++ CuoiKi/ViewModels/StagesPageViewModel.cs
@@ -15,7 +15,32 @@ namespace CuoiKi.ViewModels
         {
             _controller = new KpiController();
             _stageList = _controller.GetStagesOfProject(TaskAssignmentState.SelectedProject!) ?? new List<Stage>();
+
+            // 
+            if (TaskAssignmentState.SelectedStage == null)
+            {
+                Title = "Add New Stage";
+                StageID = "";
+                _tobeSavedStageDescription = "";
+                ShowID = Visibility.Collapsed;
+            }
+            else
+            {
+                Title = "Edit Stage";
+                StageID = TaskAssignmentState.SelectedStage!.ID;
+                _tobeSavedStageDescription = TaskAssignmentState.SelectedStage!.Description;
+                ShowID = Visibility.Visible;
+            }
+            ProjectID = TaskAssignmentState.SelectedProject!.ID;
         }
+
+        public Visibility ShowID { get; set; }
+        public string StageID { get; set; }
+        public string Title { get; set; }
+        public string ProjectID { get; set; }
+
+
+        #region Stage list binding
         private List<Stage> _stageList;
         public List<Stage> StageList
         {
@@ -26,6 +51,7 @@ namespace CuoiKi.ViewModels
                 OnPropertyChanged(nameof(StageList));
             }
         }
+        #endregion
 
         #region Save stage logic
         private string _tobeSavedStageDescription = "";
@@ -40,10 +66,23 @@ namespace CuoiKi.ViewModels
             }
         }
 
+        private void SaveStageToDB()
+        {
+            if (ToBeSavedStageDescription.Length == 0) return;
+            Stage sToSave;
+            if (StageID.Length == 0)
+            {
+                sToSave = new Stage(ProjectID, ToBeSavedStageDescription);
+            }
+            else sToSave = new Stage(StageID, ProjectID, ToBeSavedStageDescription);
+            _controller.Save(sToSave);
+        }
+
         public void SaveStage()
         {
             // Save logic here
-            MessageBox.Show(ToBeSavedStageDescription);
+            //MessageBox.Show(ToBeSavedStageDescription);
+            SaveStageToDB();
         }
 
         private bool canSaveStage = false;
@@ -65,5 +104,6 @@ namespace CuoiKi.ViewModels
         }
 
         #endregion
+
     }
 }
