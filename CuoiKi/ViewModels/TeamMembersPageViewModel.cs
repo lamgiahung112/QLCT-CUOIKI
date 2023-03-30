fb18e311791690dd59865c6fcc677bda92c0164a
bananaback <votrongtin882003@gmail.com>
2023-03-31 02:17:38 +0700

    Complete the leader infomation binding. Add some fake data to view the member list

--- CuoiKi/ViewModels/TeamMembersPageViewModel.cs
+++ CuoiKi/ViewModels/TeamMembersPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CuoiKi.Controllers;
 using CuoiKi.Models;
 using CuoiKi.States;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
@@ -10,7 +11,8 @@ namespace CuoiKi.ViewModels
     public class TeamMembersPageViewModel : ViewModelBase
     {
         private KpiController _controller;
-
+        private string _CurrentLeaderID = "";
+        private string _CurrentLeaderName = "";
         public TeamMembersPageViewModel()
         {
             _controller = new KpiController();
@@ -20,15 +22,40 @@ namespace CuoiKi.ViewModels
         private void InitializeVariables()
         {
             TeamMemberListInit();
+            Employee? currentLeader = _controller.GetTechLeadOfTeam(TaskAssignmentState.SelectedTeam!);
+            if (currentLeader is not null)
+            {
+                CurrentLeaderID = currentLeader.ID;
+                CurrentLeaderName = currentLeader.Name;
+            }
+
         }
+        #region Team member list 
         void TeamMemberListInit()
         {
+            _TeamMemberList = new ObservableCollection<Employee>();
+            _TeamMemberList.Clear();
             MessageBox.Show(TaskAssignmentState.SelectedTeam.Name);
             List<TeamMember>? teamMembers = _controller.GetAllMembersOfTeam(TaskAssignmentState.SelectedTeam!);
             foreach (TeamMember member in teamMembers)
             {
-                TeamMemberList.Add(_controller.GetTeamMemberDetails(member));
+                _TeamMemberList.Add(_controller.GetTeamMemberDetails(member));
             }
+            // Temporary add some fake data to view some team members
+            // Just curious, the app freeze a little when add a bunch of employee
+            // I wonder is it because Bcrypt hash the password...
+            _TeamMemberList.Add(new Employee("Nguyen Van A", "Ho Chi Minh", DateTime.Now, Constants.EmployeeStatus.Active, "123", Constants.Gender.Male, Constants.Role.Dev));
+            _TeamMemberList.Add(new Employee("Nguyen Van B", "Ho Chi Minh", DateTime.Now, Constants.EmployeeStatus.Active, "123", Constants.Gender.Male, Constants.Role.Dev));
+            _TeamMemberList.Add(new Employee("Nguyen Van C", "Ho Chi Minh", DateTime.Now, Constants.EmployeeStatus.Active, "123", Constants.Gender.Male, Constants.Role.Dev));
+            _TeamMemberList.Add(new Employee("Nguyen Van D", "Ho Chi Minh", DateTime.Now, Constants.EmployeeStatus.Active, "123", Constants.Gender.Male, Constants.Role.Dev));
+            _TeamMemberList.Add(new Employee("Nguyen Van E", "Ho Chi Minh", DateTime.Now, Constants.EmployeeStatus.Active, "123", Constants.Gender.Male, Constants.Role.Dev));
+            _TeamMemberList.Add(new Employee("Nguyen Van F", "Ho Chi Minh", DateTime.Now, Constants.EmployeeStatus.Active, "123", Constants.Gender.Male, Constants.Role.Dev));
+            _TeamMemberList.Add(new Employee("Nguyen Van G", "Ho Chi Minh", DateTime.Now, Constants.EmployeeStatus.Active, "123", Constants.Gender.Male, Constants.Role.Dev));
+            _TeamMemberList.Add(new Employee("Nguyen Van H", "Ho Chi Minh", DateTime.Now, Constants.EmployeeStatus.Active, "123", Constants.Gender.Male, Constants.Role.Dev));
+            _TeamMemberList.Add(new Employee("Nguyen Van I", "Ho Chi Minh", DateTime.Now, Constants.EmployeeStatus.Active, "123", Constants.Gender.Male, Constants.Role.Dev));
+            _TeamMemberList.Add(new Employee("Nguyen Van J", "Ho Chi Minh", DateTime.Now, Constants.EmployeeStatus.Active, "123", Constants.Gender.Male, Constants.Role.Dev));
+            _TeamMemberList.Add(new Employee("Nguyen Van K", "Ho Chi Minh", DateTime.Now, Constants.EmployeeStatus.Active, "123", Constants.Gender.Male, Constants.Role.Dev));
+            _TeamMemberList.Add(new Employee("Nguyen Van L", "Ho Chi Minh", DateTime.Now, Constants.EmployeeStatus.Active, "123", Constants.Gender.Male, Constants.Role.Dev));
         }
         private ObservableCollection<Employee> _TeamMemberList;
         public ObservableCollection<Employee> TeamMemberList
@@ -40,5 +67,26 @@ namespace CuoiKi.ViewModels
                 OnPropertyChanged(nameof(TeamMemberList));
             }
         }
+        #endregion
+        #region Leader information property
+        public string CurrentLeaderID
+        {
+            get { return _CurrentLeaderID; }
+            set
+            {
+                _CurrentLeaderID = value;
+                OnPropertyChanged(nameof(CurrentLeaderID));
+            }
+        }
+        public string CurrentLeaderName
+        {
+            get { return _CurrentLeaderName; }
+            set
+            {
+                _CurrentLeaderName = value;
+                OnPropertyChanged(nameof(CurrentLeaderName));
+            }
+        }
+        #endregion
     }
 }
