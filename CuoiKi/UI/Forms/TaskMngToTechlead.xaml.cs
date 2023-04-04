56a4687032429c443e1f0d8482659b41c503ea8c
Vic <thangson62@gmail.com>
2023-04-04 15:39:34 +0700

    re-edit UI mngForm, staff Form, TaskMngToTechlead

--- CuoiKi/UI/Forms/TaskMngToTechlead.xaml.cs
+++ CuoiKi/UI/Forms/TaskMngToTechlead.xaml.cs
@@ -23,5 +23,44 @@ namespace CuoiKi.UI.Forms
         {
             InitializeComponent();
         }
+
+        private void Btn_Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Btn_Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+        public bool IsMaximized = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (IsMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1080;
+                    this.Height = 720;
+
+                    IsMaximized = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+
+                    IsMaximized = true;
+                }
+            }
+        }
     }
 }
