a9c07ab21dd09ce6c5e674a11208904f7aa861af
bananaback <votrongtin882003@gmail.com>
2023-04-25 10:33:58 +0700

    add staff task management form

---
+++ CuoiKi/UI/Staff/StaffTaskManagementForm.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CuoiKi.UI.Staff
+{
+    /// <summary>
+    /// Interaction logic for StaffTaskManagementForm.xaml
+    /// </summary>
+    public partial class StaffTaskManagementForm : Page
+    {
+        public StaffTaskManagementForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
