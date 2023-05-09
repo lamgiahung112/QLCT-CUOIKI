e196747702c2d66d4a2a8bc33b49511c06dda2a8
Vic <thangson62@gmail.com>
2023-05-09 13:45:13 +0700

    MemberList, ViewSalary

---
+++ CuoiKi/UI/HumanResources/MemberList.xaml.cs
@@ -0,0 +1,35 @@
+﻿using CuoiKi.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CuoiKi.UI.HumanResources
+{
+    /// <summary>
+    /// Interaction logic for MemberList.xaml
+    /// </summary>
+    public partial class MemberList : Page
+    {
+        public MemberList()
+        {
+            InitializeComponent();
+            this.DataContext = new SalaryOfMember();
+        }
+
+        private void btn_ViewSalary_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ViewSalary());
+        }
+    }
+}
